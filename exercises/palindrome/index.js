dcd024cfaaa2cbc53ad46ca4e02311307952512f
Mithun Gudipati <mgudi02@safeway.com>
2020-04-28 17:36:02 -0700

    palindrome with permutations

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,7 +7,38 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {
+
+function palindrome (str){
+//Permuations of palindrome problem.
+// Basically we cut the palindrome in 2 halfs ... hashmap every character, check if any of them are odd.
+
+arr = str.split("");
+console.log(arr);
+var result = arr.reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {});
+counter = 0;
+for(let a in result){
+  if(result[a]%2 === 0){
+    //do nothing
+  }else{
+    counter++;
+  }
+}
+if(counter<2){console.log("permutation palindrome is true")}else {
+  console.log("permutation palindrome is not true")
+}
+return counter<2;
+}
+palindrome("ivicc");
+
+function palindrome1(str){
+ return str=== str.split("").reduce((reversed, character)=>{
+   return character+reversed;
+ },"")
+
+}
+
+
+function palindrome2(str) {
 
 if (str === str.split("").reduce((reversed, character)=>{
   return character+reversed;
